9dad724413899a8dbe23c1a075c89829bbfcb6ff
Eric Amodeo <ecamodeo@gmail.com>
2017-10-01 12:42:13 -0400

    Updates to quidditch checks

--- quidditch/check50/__init__.py
+++ quidditch/check50/__init__.py
@@ -1,28 +1,38 @@
 from check50 import *
 
-class Room(Checks):
+class quidditch(Checks):
 
     @check()
     def exists(self):
-        """room.c exists"""
-        self.require("room.c")
+        """quidditch.c exists"""
+        self.require("quidditch.c")
 
     @check("exists")
     def compiles(self):
-        """room.c compiles"""
-        self.spawn("clang -o room room.c -lcs50 -lm").exit(0)
+        """quidditch.c compiles"""
+        self.spawn("clang -o quidditch quidditch.c -lcs50 -lm").exit(0)
 
     @check("compiles")
-    def test_A_and_7(self):
-        """Letter A, room 7 yeilds A7"""
-        self.spawn("./room").stdin("A").stdin("7").stdout("A7\n", "A7\n").exit(0)
+    def five_quaffle_through_hoop_snitch(self):
+        """5 Quaffles through the other team's hoops, got the snitch"""
+        self.spawn("./quidditch").stdin("5").stdin("1").stdout("200\n", "200\n").exit(0)
 
     @check("compiles")
-    def test_invalid_letters(self):
-        """rejects letters beyond G"""
-        self.spawn("./room").stdin("L").reject().stdin("M").reject()
+    def two_quaffle_through_hoop_no_snitch(self):
+        """2 Quaffles through the other team's hoops, no snitch"""
+        self.spawn("./quidditch").stdin("2").stdin("0").stdout("20\n", "20\n").exit(0)
+
+    @check("compiles")
+    def test_invalid_quaffles(self):
+        """rejects non ints and bools"""
+        self.spawn("./quidditch").stdin("L").reject().stdin("3").reject()
+
+    @check("compiles")
+    def test_invalid_quaffles2(self):
+        """rejects non ints and bools"""
+        self.spawn("./quidditch").stdin("1.2").reject().stdin("M").reject()
 
     @check("compiles")
     def test_invalid_integers(self):
         """rejects numbers < 1 or > 20"""
-        self.spawn("./room").stdin("-8").reject().stdin("35").reject().stdin("100").reject()
+        self.spawn("./quidditch").stdin("-8").reject().stdin("35").reject().stdin("100").reject()
