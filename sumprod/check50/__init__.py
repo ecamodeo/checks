201623cc2f191ac1ad10f1ce5d5cf20c2d262edb
Eric Amodeo <ecamodeo@gmail.com>
2017-10-04 09:29:26 -0400

    Updates to sumprod check

--- sumprod/check50/__init__.py
+++ sumprod/check50/__init__.py
@@ -11,3 +11,13 @@ class Sumprod(Checks):
     def compiles(self):
         """sumprod.c compiles"""
         self.spawn("clang -o sumprod sumprod.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test_10_and_5(self):
+        """10 and 5 yeilds 15 and 50"""
+        self.spawn("./sumprod").stdin("10").stdin("5").stdout("15\n", "15\n")..stdout("50\n", "50\n")exit(0)
+
+    @check("compiles")
+    def test_non_ints(self):
+        """rejects non ints"""
+        self.spawn("./sumprod").stdin("L").reject().stdin("3.2").reject()
