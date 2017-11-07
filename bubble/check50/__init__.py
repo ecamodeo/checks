825b2ca5bbfe7bb37b3507ea2f0bc92992b4af40
Eric Amodeo <ecamodeo@gmail.com>
2017-11-07 08:24:02 -0500

    Added bubble

---
+++ bubble/check50/__init__.py
@@ -0,0 +1,18 @@
+from check50 import *
+
+class Bubble(Checks):
+
+    @check()
+    def exists(self):
+        """bubble.c exists"""
+        self.require("bubble.c")
+
+    @check("exists")
+    def compiles(self):
+        """bubble.c compiles"""
+        self.spawn("clang -o bubble bubble.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test_bubble_sort(self):
+        """Sorts as 4 15 16 50 8 23 42 108"""
+        self.spawn("./bubble")..stdout("4 15 16 50 8 23 42 108\n", "4 15 16 50 8 23 42 108\n").exit(0)
