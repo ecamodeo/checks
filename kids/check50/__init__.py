5e87ba5f96139c62afd27d0eb63e3a751a845102
Eric Amodeo <ecamodeo@gmail.com>
2017-11-13 09:31:42 -0500

    Issues with my check

--- kids/check50/__init__.py
+++ kids/check50/__init__.py
@@ -12,4 +12,9 @@ class kids(Checks):
         """kids.c compiles"""
         self.spawn("clang -o kids kids.c -lcs50 -lm").exit(0)
 
+    @check("compiles")
+    def test_1(self):
+        """Input of 1 outputs That is correct!""""
+        self.spawn("./kids").stdin("1").stdout("That is incorrect!\n", "That is incorrect!\n").exit(0)
+
     
