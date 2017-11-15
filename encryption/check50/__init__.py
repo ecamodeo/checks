e5cc630e8ac8d211c7e3229ba7831861d596ed7d
Eric Amodeo <ecamodeo@gmail.com>
2017-11-14 22:42:44 -0500

    …

--- encryption/check50/__init__.py
+++ encryption/check50/__init__.py
@@ -16,8 +16,3 @@ class encryption(Checks):
     def test_seven(self):
         """Entering 154 and 209 passes"""
         self.spawn("./encryption").stdin("154").stdin("209").stdout("Success! Message was encrypted\n", "Success! Message was encrypted\n").exit(0)
-
-    @check("compiles")
-    def test_seven(self):
-        """Entering 13 and 201 fails"""
-        self.spawn("./encryption").stdin("13").stdin("201").stdout("Factors failed. Please try again later\n", "Factors failed. Please try again later\n").exit(0)
