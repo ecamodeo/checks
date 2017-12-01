410d4233a3aad047a5a37c0a061c74f208c76270
Eric Amodeo <ecamodeo@gmail.com>
2017-12-01 08:59:52 -0500

    …

--- bubble/check50/__init__.py
+++ bubble/check50/__init__.py
@@ -17,4 +17,4 @@ class Bubble(Checks):
     def test_bubble_sort(self):
         """Sorts as 4 8 15 16 23 42 50 108"""
         #out = self.spawn("./bubble").stdout(File("bubble.txt")).exit(0)
-        self.spawn("./bubble").stdout(File("bubble.txt")).exit(0)
+        self.spawn("./bubble").stdout("4 15 16 50 8 23 42 108 \n4 8 15 16 23 42 50 108 \n", "4 15 16 50 8 23 42 108 \n4 8 15 16 23 42 50 108 \n").exit(0)
