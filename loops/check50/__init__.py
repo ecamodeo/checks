a46c977364102019410b1f9c575847cb83b70cdf
Eric Amodeo <ecamodeo@gmail.com>
2018-01-08 09:55:50 -0500

    …

--- loops/check50/__init__.py
+++ loops/check50/__init__.py
@@ -15,9 +15,9 @@ class loops(Checks):
     @check("compiles")
     def value_of_17(self):
         """17 produces 17, 34, 51, 68, ..., 170"""
-        self.spawn("./loops").stdin("17").stdout("200\n", "200\n").exit(0)
+        self.spawn("./loops").stdin("17").stdout("17\n34\n51\n68\n85\n102\n119\n136\n153\n170\n", "17\n34\n51\n68\n85\n102\n119\n136\n153\n170\n").exit(0)
 
     @check("compiles")
     def test_invalid_entries(self):
-        """rejects invalid entries"""17	34	51	68	85	102	119	136	153	170
+        """rejects invalid entries"""
         self.spawn("./loops").stdin("L").reject().stdin("300").reject().stdin("0").reject().stdin("-7").reject()
