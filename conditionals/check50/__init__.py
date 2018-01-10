df2aa747b0803ae0db3a93d15d53758e9ef479ef
Eric Amodeo <ecamodeo@gmail.com>
2018-01-10 09:33:11 -0500

    added more tests to conditionals

--- conditionals/check50/__init__.py
+++ conditionals/check50/__init__.py
@@ -13,9 +13,24 @@ class conditionals(Checks):
         self.spawn("clang -o conditionals conditionals.c -lcs50 -lm").exit(0)
 
     @check("compiles")
-    def value_of_17(self):
-        """Grades of 93, 88, and 77 result in Second Honors and average of 86.0"""
+    def first_honors(self):
+        """Grades of 98, 90, and 94 result in First Honors"""
+        self.spawn("./conditionals").stdin("98").stdin("90").stdin("94").stdout("First Honors\n", "First Honors\n").exit(0)
+
+    @check("compiles")
+    def second_honors(self):
+        """Grades of 93, 88, and 77 result in Second Honors"""
         self.spawn("./conditionals").stdin("93").stdin("88").stdin("77").stdout("Second Honors\n", "Second Honors\n").exit(0)
+
+    @check("compiles")
+    def third_honors(self):
+        """Grades of 82, 84, and 80 result in Third Honors"""
+        self.spawn("./conditionals").stdin("82").stdin("84").stdin("80").stdout("Third Honors\n", "Third Honors\n").exit(0)
+
+    @check("compiles")
+    def no_honors(self):
+        """Grades of 65, 50, and 70 result in Not good enough!"""
+        self.spawn("./conditionals").stdin("65").stdin("50").stdin("70").stdout("Not good enough!\n", "Not good enough!\n").exit(0)
 
     @check("compiles")
     def test_invalid_entries(self):
